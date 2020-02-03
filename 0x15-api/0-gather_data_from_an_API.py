3c4230e678a76aab149a17ac4232a194a82528c5
Miuywu <859@holbertonschool.com>
2020-02-03 15:52:55 -0800

    works with argv now

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,15 +1,32 @@
 #!/usr/bin/python3
 """ uses REST API to take in employee ID returns TODO list info """
 
-#from urllib import *
+# from urllib import *
 import requests
 from sys import argv
 
 if __name__ == "__main__":
-    req = requests.get('https://intranet.hbtn.io/status')
-    name = https://jsonplaceholder.typicode.com/users
+    strId = argv[1]# employee ID number
+
+    user = requests.get("https://jsonplaceholder.typicode.com/users/" + strId).json()
+    name = user['name']
+
+    all_todos = requests.get("https://jsonplaceholder.typicode.com/todos").json()
+    totalTasks = 0
+    tasksDone = 0
+    for todo in all_todos:
+        if str(todo['userId']) == strId:
+            totalTasks += 1
+            if todo['completed'] == True:
+                tasksDone += 1
 
     first_line = 'Employee {} is done with tasks({}/{}):'.format(name,
                                                                  tasksDone,
-                                                                 total_tasks)
+                                                                 totalTasks)
     print(first_line)
+    for todo in all_todos:
+        if str(todo['userId']) == strId:
+            if todo['completed'] == True:
+                title = todo['title']
+                print("\t" + title)
+
