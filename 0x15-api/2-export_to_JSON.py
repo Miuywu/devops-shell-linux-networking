0767ac1bc364620866c876efa978a1ea7d5c46fb
Miuywu <859@holbertonschool.com>
2020-02-03 18:08:49 -0800

    writes todo list to json

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" uses REST API to take in employee ID returns TODO list info """
+
+import json
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    strId = argv[1]
+    rURL = "https://jsonplaceholder.typicode.com/users/"
+    user = requests.get(rURL + strId).json()
+    userName = user.get('username')
+
+    rURL2 = "https://jsonplaceholder.typicode.com/todos"
+    all_todos = requests.get(rURL2).json()
+
+    todo_list = []
+    for todo in all_todos:
+        list_entry = {} 
+        if str(todo.get('userId')) == strId:
+            taskStatus = str(todo.get('completed'))
+            title = todo.get('title')
+            list_entry["task"] = title
+            list_entry["completed"] = taskStatus
+            list_entry["username"] = userName
+            todo_list.append(list_entry)
+    
+    employeeDict = {strId: todo_list}
+    derulo = strId + ".json"
+    with open(derulo, 'w', newline='') as file:
+        file.write(json.dumps(employeeDict))
