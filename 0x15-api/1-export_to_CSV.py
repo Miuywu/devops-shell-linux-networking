29a1794d6e00781db1e3fc9df8ee9d404cda569d
Miuywu <859@holbertonschool.com>
2020-02-03 17:45:27 -0800

    writes todo list into csv file

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" uses REST API to take in employee ID returns TODO list info """
+
+import csv
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    strId = argv[1]
+    rURL = "https://jsonplaceholder.typicode.com/users/"
+    user = requests.get(rURL + strId).json()
+    userName = user.get('username')
+
+    rURL2 = "https://jsonplaceholder.typicode.com/todos"
+    all_todos = requests.get(rURL2).json()
+
+    csvFile = strId + ".csv"
+    with open(csvFile, 'w', newline='') as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_NONNUMERIC,)
+        for todo in all_todos:
+            if str(todo.get('userId')) == strId:
+                taskStatus = todo.get('completed')
+                title = todo.get('title')
+                writer.writerow([strId, userName, taskStatus, title])
