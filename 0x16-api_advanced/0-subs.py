39d1287a41fd5c86e4d63c4f6049def0a3398971
Miuywu <859@holbertonschool.com>
2020-02-06 15:53:26 -0800

    refactor

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,8 @@ def number_of_subscribers(subreddit):
     rURL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     h = {"User-Agent": 'any agent'}
 
-    data = requests.get(rURL, h, allow_redirects=False).json().get('data')
+    data = requests.get(rURL, headers=h, allow_redirects=False).json()
     if data is None:
         return 0
-    return data.get('subscribers')
+    data2 = data.get('data')
+    return data2.get('subscribers')
